debc360edd5cc92ab937bb240fd129f96125c81f
aryan79842716 <63910017+aryan79842716@users.noreply.github.com>
2020-11-25 05:57:56 +0700

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by . Aryan on 15/04/20.
+//  Copyright © 2020 . Aryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
